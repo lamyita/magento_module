2176f86fbdf28688bf22003814626814787a36eb
lamyita <50775487+lamyita@users.noreply.github.com>
2021-06-23 23:00:31 +0100

    create controller and action

---
+++ app/code/Egio/ControllerEgio/Block/Index.php
@@ -0,0 +1,37 @@
+<?php
+namespace Egio\ControllerEgio\Block;
+use Egio\ControllerEgio\Model\ResourceModel\Post\Collection;
+use Magento\Framework\View\Element\Template;
+
+// The Block file should contain all the view logic required, 
+// it should not contain any kind of html or css. Block file are supposed to have all application view logic.
+// Every block in Magento 2 must extend from Magento\Framework\View\Element\Template
+//create a block to inject logic into your Magento2 templates. 
+namespace Egio\ControllerEgio\Block;
+class Index extends \Magento\Framework\View\Element\Template
+{
+
+
+
+
+
+//     protected $_filesystem;
+ 
+//      public function __construct(
+//           \Magento\Framework\View\Element\Template\Context $context,
+//           Egio\ControllerEgio\Model\PostFactory $postFactory
+//           //  Egio\ControllerEgio\Model\PostFactory $postFactory
+//           )
+//      {
+//           parent::__construct($context);
+//           $this->_postFactory = $postFactory;
+//      }
+ 
+//      public function getResult()
+//      {
+//           $post = $this->_postFactory->create();
+//           $collection = $post->getCollection();
+//           return $collection;
+//      }
+    
+}
