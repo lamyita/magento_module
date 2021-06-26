af8bf2167ebae6b85823cdca2a547d75040ae7b2
lamyita <50775487+lamyita@users.noreply.github.com>
2021-06-26 11:57:44 +0100

    files for edit

---
+++ app/code/Egio/EgioReinsurance/Block/Adminhtml/Reviews/Edit.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Egio\EgioReinsurance\Block\Adminhtml\Reviews;
+/**
+ * Class Edit
+ * @package Egio\EgioReinsurance\Block\Adminhtml\Reviews
+ */
+class Edit extends \Magento\Backend\Block\Widget\Form\Container
+{
+    /**
+     * Core registry.
+     *
+     * @var \Magento\Framework\Registry
+     */
+    protected $_coreRegistry = null;
+
+    /**
+     * @param \Magento\Backend\Block\Widget\Context $context
+     * @param \Magento\Framework\Registry $registry
+     * @param array $data
+     */
+    public function __construct(\Magento\Backend\Block\Widget\Context $context, \Magento\Framework\Registry $registry, array $data = [])
+    {
+        $this->_coreRegistry = $registry;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     *
+     */
+    protected function _construct()
+    {
+        $this->_objectId = 'reassurance_id';
+        $this->_blockGroup = 'Egio_EgioReinsurance';
+        $this->_controller = 'adminhtml_reviews';
+        parent::_construct();
+        if ($this->_isAllowedAction('Egio_EgioReinsurance::edit')) {
+            $this->buttonList->update('save', 'label', __('Save'));
+        } else {
+            $this->buttonList->remove('save');
+        }
+        $this->buttonList->remove('reset');
+    }
+
+    /**
+     * Retrieve text for header element depending on loaded image.
+     *
+     * @return \Magento\Framework\Phrase
+     */
+    public function getHeaderText()
+    {
+        return __('Edit reinsurance');
+    }
+
+    /**
+     * Check permission for passed action.
+     *
+     * @param string $resourceId
+     *
+     * @return bool
+     */
+    protected function _isAllowedAction($resourceId)
+    {
+        return $this->_authorization->isAllowed($resourceId);
+    }
+
+    /**
+     * Get form action URL.
+     *
+     * @return string
+     */
+    public function getFormActionUrl()
+    {
+        if ($this->hasFormActionUrl()) {
+            return $this->getData('form_action_url');
+        }
+        return $this->getUrl('*/*/save');
+    }
+}
