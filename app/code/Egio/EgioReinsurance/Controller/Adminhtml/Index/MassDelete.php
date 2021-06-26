98b75816ff7673188e47e3dff6f690fe288ecf71
lamyita <50775487+lamyita@users.noreply.github.com>
2021-06-26 11:57:29 +0100

    add message delete

---
+++ app/code/Egio/EgioReinsurance/Controller/Adminhtml/Index/MassDelete.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Egio\EgioReinsurance\Controller\Adminhtml\Index;
+/**
+ * Class MassDelete
+ * @package Egio\EgioReinsurance\Controller\Adminhtml\Index
+ */
+class MassDelete extends \Magento\Backend\App\Action
+{
+    protected $_reviewsFactory;
+
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Egio\EgioReinsurance\Model\ReviewsFactory $reviewsFactory
+    )
+    {
+        $this->_reviewsFactory = $reviewsFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+     * @SuppressWarnings(PHPMD.NPathComplexity)
+     */
+    public function execute()
+    {
+        $data = $this->getRequest()->getPostValue();
+        $selectedIds = $data['selected'];
+        try {
+            foreach ($selectedIds as $selectedId) {
+                $deleteData = $this->_reviewsFactory->create()->load($selectedId);
+                $deleteData->delete();
+            }
+            $this->messageManager->addSuccess(__('Row data has been successfully deleted.'));
+        } catch (\Exception $e) {
+            $this->messageManager->addError(__($e->getMessage()));
+        }
+        $this->_redirect('md_cr/index/index');
+    }
+
+    /**
+     * @return bool
+     */
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('Egio_EgioReinsurance::home');
+    }
+}
